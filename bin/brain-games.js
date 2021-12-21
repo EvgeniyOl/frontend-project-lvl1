9bc26a0b42ec9bd47b41e73648aabaa93b3e42ad
Евгений Олянский <evgeniy.olyanskii@gmail.com>
2021-12-21 15:54:49 +0600

    linter install

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 import userName from '/Users/evgeniy/Project/frontend-project-lvl1/src/cli.js';
-console.log("Welcome to the Brain Games!");
+
+console.log('Welcome to the Brain Games!');
 userName();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
+
 const userName = () => {
-const name = readlineSync.question('What is your name?: ');
-console.log("Hello, " + name + "!");
+  const name = readlineSync.question('What is your name?: ');
+  console.log(`Hello, ${name}!`);
 };
 export default userName;
