53ebd7c8dd6792c7cebd3eb8a1375fc1e4e3b86f
Евгений Олянский <evgeniy.olyanskii@gmail.com>
2021-12-21 21:44:56 +0600

    ../

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import userName from '/Users/evgeniy/Project/frontend-project-lvl1/src/cli.js';
-
+import userName from '../src/cli.js';
 console.log('Welcome to the Brain Games!');
 userName();
