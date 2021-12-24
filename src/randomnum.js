97e462e3d40d61257166d04d1abb3e544231d91e
Евгений Олянский <evgeniy.olyanskii@gmail.com>
2021-12-24 22:34:32 +0600

    minerror

--- src/randomnum.js
+++ src/randomnum.js
@@ -1,6 +1,8 @@
-const getRandomNumber = (min, max) => {
+function getRandomNumber(min, max) {
+  // eslint-disable-next-line no-param-reassign
   min = Math.ceil(min);
+  // eslint-disable-next-line no-param-reassign
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+}
 export default getRandomNumber;
