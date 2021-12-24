de3e204854f482cd3c08c6f16dbe8981fffb0cb0
Евгений Олянский <evgeniy.olyanskii@gmail.com>
2021-12-24 21:44:49 +0600

    ff

--- src/progression-logic.js
+++ src/progression-logic.js
@@ -6,13 +6,13 @@ const progrLength = 10;
 
 const createProgression = (firstNumber, step, length) => {
   const progression = [];
-  for (let i = 1; i < length; i += 1) {
+  for (let i = 0; i < length; i += 1) {
     progression.push(firstNumber + step * i);
   }
   return progression;
 };
 const gameCheck = () => {
-  const step = getRandomNumber(1, 5);
+  const step = getRandomNumber(1, 50);
   const firstNumber = getRandomNumber(1, 10);
   const progression = createProgression(firstNumber, step, progrLength);
   const randomNumber = getRandomNumber(0, progression.length - 1);
@@ -20,7 +20,7 @@ const gameCheck = () => {
   progression[randomNumber] = '..';
   const question = progression;
   const checkAnswer = `${result}`;
-  return [question, checkAnswer];
+  return [String(question), checkAnswer];
 };
 const pusk = () => startGame(gameRules, gameCheck);
 
