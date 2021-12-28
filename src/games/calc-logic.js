efd11a34de89f54b432c90388443251ff562a280
Евгений Олянский <evgeniy.olyanskii@gmail.com>
2021-12-28 17:45:21 +0600

    calc correct

--- src/games/calc-logic.js
+++ src/games/calc-logic.js
@@ -1,29 +1,31 @@
 import getRandomNumber from '../getRandomNumber.js';
 import startGame from '../index.js';
 
-const gameRules = 'What is the result of the expression?'; // правила игры
+const gameRules = 'What is the result of the expression?';
+
+const getRandomOperator = () => {
+  const Operator = ['+', '-', '*'];
+  return Operator[Math.floor(Math.random() * Operator.length)];
+};
+function resultOfCalc(number1, number2, operator) {
+  let result = 0;
+  if (operator === '+') {
+    result = number1 + number2;
+  } else if (operator === '-') {
+    result = number1 - number2;
+  } else {
+    result = number1 * number2;
+  }
+  return result;
+}
 
 const gameCheck = () => {
   const num1 = getRandomNumber(1, 10); // первое число
   const num2 = getRandomNumber(1, 10); // второе число
-  const randomOperator = ['+', '-', '*']; // список операторов
-  const ranOpIndex = Math.floor(Math.random() * 3); // рандомный индекс
-  const operator = randomOperator[ranOpIndex]; // случайный оператор
-  let result;
-  switch (ranOpIndex) {
-    case 0:
-      result = num1 + num2;
-      break;
-    case 1:
-      result = num1 - num2;
-      break;
-    case 2:
-      result = num1 * num2;
-      break;
-    default:
-  }
-  const question = `${num1} ${operator} ${num2}`;
-  const checkAnswer = `${result}`;
+  const randomOperator = getRandomOperator();
+  const question = `${num1} ${randomOperator} ${num2}`;
+  const calcResult = resultOfCalc(num1, num2, randomOperator);
+  const checkAnswer = `${calcResult}`;
   return [question, checkAnswer];
 };
 const pusk = () => startGame(gameRules, gameCheck);
